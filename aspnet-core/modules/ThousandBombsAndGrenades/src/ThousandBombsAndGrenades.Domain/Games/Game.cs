b1b96a336ad6dbc14ef43a9db5c94e21175c605b
Steff Beckers <steff@steffbeckers.eu>
2021-05-16 15:17:07 +0200

    TBAG game aggregate added

---
+++ aspnet-core/modules/ThousandBombsAndGrenades/src/ThousandBombsAndGrenades.Domain/Games/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace ThousandBombsAndGrenades.Games
+{
+    public class Game : FullAuditedAggregateRoot<Guid>
+    {
+        public virtual DateTime? StartDate { get; set; }
+
+        public virtual DateTime? EndDate { get; set; }
+
+        public Game()
+        {
+        }
+
+        public Game(Guid id, DateTime? startDate = null, DateTime? endDate = null)
+        {
+            Id = id;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+    }
+}
---
+++ aspnet-core/modules/ThousandBombsAndGrenades/src/ThousandBombsAndGrenades.Domain/Games/IGameRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Volo.Abp.Domain.Repositories;
+
+namespace ThousandBombsAndGrenades.Games
+{
+    public interface IGameRepository : IRepository<Game, Guid>
+    {
+    }
+}
